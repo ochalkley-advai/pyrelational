ab089eb698afa2876bd0fd5cad8c296ffc2e300c
Oliver Chalkley <oliver.chalkley@advai.co.uk>
2024-03-11 20:56:45 +0000

    added coco dataset class

--- examples/demo/utils/datasets.py
+++ examples/demo/utils/datasets.py
@@ -1,9 +1,58 @@
 """
 Simple datasets in PyTorch to use in examples
 """
+import json
+import os
+
 import torch
+from PIL import Image
 from sklearn.datasets import load_breast_cancer, load_diabetes
 from torch.utils.data import Dataset
+
+
+class COCODataset(Dataset):
+    """
+    Dataset class for COCO-style object detection.
+    """
+
+    def __init__(self, image_dir, annotation_file, transform=None):
+        """
+        Args:
+            image_dir (str): Directory with all the images.
+            annotation_file (str): Path to the JSON file with annotations.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.image_dir = image_dir
+        self.transform = transform
+        with open(annotation_file) as f:
+            self.annotations = json.load(f)
+        self.images = self.annotations["images"]
+        self.categories = {category["id"]: category["name"] for category in self.annotations["categories"]}
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_id = self.images[idx]["id"]
+        img_path = os.path.join(self.image_dir, self.images[idx]["file_name"])
+        image = Image.open(img_path).convert("RGB")
+        annotations = [anno for anno in self.annotations["annotations"] if anno["image_id"] == img_id]
+        boxes = []
+        labels = []
+        for anno in annotations:
+            boxes.append(anno["bbox"])
+            labels.append(anno["category_id"])
+
+        sample = {
+            "image": image,
+            "boxes": torch.as_tensor(boxes, dtype=torch.float32),
+            "labels": torch.as_tensor(labels, dtype=torch.int64),
+        }
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
 
 
 class DiabetesDataset(Dataset):
